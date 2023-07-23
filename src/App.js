3d80ddf361b5fa43bd4444b9cd7f08627aae1ad7
Nijiiro <josephnaval47@gmail.com>
2023-07-24 00:01:29 +0800

    landing page modifications

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './static/css/style_config.css';
+
+import Navbar from "./component/Navbar";
+import Landing from "./pages/Landing";
+import Footer from "./component/footer";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar/>
+        <Landing />
+        <Footer />
+    </>
   );
 }
 
