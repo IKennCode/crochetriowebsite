121aa12e551781bbcbd001db5b21a78c4c0dd2e5
Nijiiro <josephnaval47@gmail.com>
2023-07-28 17:25:06 +0800

    order form data

--- src/component/multistepForm.jsx
+++ src/component/multistepForm.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, {useRef,useState,useEffect} from 'react'
 import FormWizard from "react-form-wizard-component";
 import 'react-form-wizard-component/dist/style.css';
 import '../static/css/commission.css'
 
 const MultistepForm = () => {
+
+    // const fullname_ref = useRef(null);
+    // const email = useRef(null);
+    // const phone = useRef(null);
+    // const address = useRef(null);
+    // const yarn_type_ref = useRef(null);
+    // const product_type_ref = useRef(null);
+
+    const [fullname,setFullname] = useState("");
+    const [email,setEmail] = useState("");
+    const [phone,setPhone] = useState("");
+    const [address,setAddress] = useState("");
+    const [yarn_type,setYarnType] = useState("");
+    const [product_type,setProductType] = useState("");
+    const [payment_method,setPaymentMethod] = useState("e_wallet");
+    const [service_amount,setServiceAmount] = useState(3000);
+    const [product_amount,setProductAmount] = useState(0);
+    const [total_amount,setTotalAmount] = useState(0);
+
+    const form_data = {}
+
     const handleComplete = () => {
         console.log("Form completed!");
+        console.log(form_data);
       };
+
+
+    useEffect(() =>{
+
+        form_data.fullname = fullname;
+        form_data.email = email;
+        form_data.phone = phone;
+        form_data.address = address;
+        form_data.yarn_type = yarn_type;
+        form_data.product_type = product_type;
+        form_data.payment_method = payment_method;
+        form_data.product_amount = product_amount;
+        form_data.service_amount = service_amount;
+        form_data.total_amount = service_amount + product_amount;
+        
+
+    }, [form_data])
+
+
       return (
-        <>
             <div className="step-form container mt-5 px-5 w-75 rounded-3 b-shadow">
                     <FormWizard
                     onComplete={handleComplete}
@@ -20,12 +60,12 @@ const MultistepForm = () => {
                     <FormWizard.TabContent title="Personal details" icon="ti-user">
                         <form className='tab-form d-flex flex-row justify-content-center column-gap-4 mt-4'>
                             <div className="form-group d-flex flex-column row-gap-2">
-                                <input type="text" className='form-control' name="fullname" id="fullname" placeholder='Full Name'/>
-                                <input type="number" className='form-control' name="phone" id="phone" placeholder='Phone Number' />
+                                <input type="text" onChange={(e) => setFullname(e.target.value)} className='form-control' name="fullname" id="fullname" placeholder='Full Name'/>
+                                <input type="number" onChange={(e) => setPhone(e.target.value)} className='form-control' name="phone" id="phone" placeholder='Phone Number' />
                             </div>
                             <div className="form-group d-flex flex-column row-gap-2">
-                                <input type="email" className='form-control' name="email" id="email" placeholder='Enter Email'/>
-                                <input type="text" className='form-control' name="address" id="address" placeholder='Delivery Address' />
+                                <input type="email" onChange={(e) => setEmail(e.target.value)} className='form-control' name="email" id="email" placeholder='Enter Email'/>
+                                <input type="text" onChange={(e) => setAddress(e.target.value)} className='form-control' name="address" id="address" placeholder='Delivery Address' />
                             </div>
                         </form>
                         <hr />
@@ -36,7 +76,7 @@ const MultistepForm = () => {
                                 <input type="file" name="design" id="design" />
                             </div>
                             <div className="form-group d-flex flex-column row-gap-2">
-                                <select className='form-select' name="yarn-type" id="yarn-type">
+                                <select onChange={(e) => setYarnType(e.target.value)} className='form-select' name="yarn-type" id="yarn-type">
                                     <option value="">Yarn Type</option>
                                     <option value="wool">Wool</option>
                                     <option value="cotton">Cotton</option>
@@ -44,12 +84,24 @@ const MultistepForm = () => {
                                     <option value="crochet-thread">Crochet Thread</option>
                                 </select>
                             </div>
-                            <div className="form-group d-flex flex-column row-gap-2">
-                                <select className='form-select' name="product-type" id="product-type">
+                            <div  className="form-group d-flex flex-column row-gap-2">
+                                <select onChange={
+                                    (e) =>
+                                    {   
+
+                                        let pr_type = e.target.value.split('-')[0]
+                                        let pr_amount = parseInt(e.target.value.split('-')[1])
+
+                                        setProductType(pr_type);
+                                        setProductAmount(pr_amount);
+                                        setTotalAmount(service_amount + pr_amount);
+                                        
+                                    }} 
+                                    className='form-select' name="product-type" id="product-type">
                                     <option value="">Product Type</option>
-                                    <option value="keychain">Keychain-P500</option>
-                                    <option value="doll">Doll-P1000</option>
-                                    <option value="headwear">Headwear-P2000</option>
+                                    <option value="keychain-500">Keychain-500</option>
+                                    <option value="doll-1000">Doll-1000</option>
+                                    <option value="headwear-2000">Headwear-2000</option>
                                 </select>
                             </div>
                         </form>
@@ -59,26 +111,25 @@ const MultistepForm = () => {
                         <div className='form-review d-flex flex-column align-items-start p-3 border border-3 rounded-3'>
                             <h4 className='text-center mb-5 w-100'>-----Request Form Review-----</h4>
                             <h5>Personal Details</h5>
-                            <h6>Full Name: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Email: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Phone Number: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Delivery Address: <span className='fw-light'>Sample Value</span></h6>
+                            <h6>Full Name: <span className='fw-light'>{fullname}</span></h6>
+                            <h6>Email: <span className='fw-light'>{email}</span></h6>
+                            <h6>Phone Number: <span className='fw-light'>{phone}</span></h6>
+                            <h6>Delivery Address: <span className='fw-light'>{address}</span></h6>
                             <h5>Request Details</h5>
-                            <h6>Design Reference: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Yarn Type: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Product Type: <span className='fw-light'>Sample Value</span></h6>
+                            <h6>Design Reference: <span className='fw-light'>Sample Image</span></h6>
+                            <h6>Yarn Type: <span className='fw-light'>{yarn_type}</span></h6>
+                            <h6>Product Type: <span className='fw-light'>{product_type}</span></h6>
                             <h5>Payment Details</h5>
-                            <h6>Payment Method: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Product Amount: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Service Amount: <span className='fw-light'>Sample Value</span></h6>
-                            <h6>Total Amount: <span className='fw-light'>Sample Value</span></h6>
+                            <h6>Payment Method: <span className='fw-light'>{payment_method}</span></h6>
+                            <h6>Product Amount: <span className='fw-light'>₱{product_amount}</span></h6>
+                            <h6>Service Amount: <span className='fw-light'>₱{service_amount}</span></h6>
+                            <h6>Total Amount: <span className='fw-light'>₱{total_amount}</span></h6>
                             <h4 className='text-center mt-5 w-100'>-----To Proceed To Checkout Click "Finish"-----</h4>
+                            <hr /> 
                         </div>
-                        <hr />
                     </FormWizard.TabContent>
                     </FormWizard>
             </div>
-        </>
       );
 }
 
