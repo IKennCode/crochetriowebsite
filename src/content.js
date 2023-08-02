89a89e806e7e7d8e526425e52b6b7dc1d7c4cb85
Kenneth Malabriga Dimalibot <138239593+IKennCode@users.noreply.github.com>
2023-08-02 11:24:16 +0800

    Add files via upload

---
+++ src/content.js
@@ -0,0 +1,92 @@
+const productData = [
+    {
+      id: 1,
+      name: "Smallsize Baby Squid Crochet",
+      image:"image1.jpg",
+      productType: "Crochet",
+      sale:499,
+      price: 220,
+      rating: 5,
+      timeLeft: 27,
+      totalSales: 7479 
+    },
+    {
+      id: 2,
+      name: "Medium Size DarkBrown Bear Crochet",
+      image:"image2.jpg",
+      productType: "Crochet",
+      sale:499,
+      price: 169,
+      rating: 5,
+      timeLeft: 24,
+      totalSales: 6898
+    },
+    {
+      id: 3,
+      name: "Large Size LightBrown Bear Crochet",
+      image:"image3.jpg",
+      productType: "Crochet",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+      id: 4,
+      name: "Full Set Large Size Bear with Garden Crochet",
+      image:"image4.jpg",
+      productType: "Crochet",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+      id: 5,
+      name: "Medium Size Couple Bear and Rabbit Crochet",
+      image:"image5.jpg",
+      productType: "openBackHeadphones",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+      id: 6,
+      name: "Medium Size Pretty Bunny Crochet",
+      image:"image6.jpg",
+      productType: "openBackHeadphones",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+      id: 7,
+      name: "Christmas Celebrity Bunny Crochet",
+      image:"image7.jpg",
+      productType: "openBackHeadphones",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+      id: 8,
+      name: "Medium Size 3 Companions Crochets",
+      image:"image8.jpg",
+      productType: "openBackHeadphones",
+      sale:499,
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+
+  ];
+  export default productData;
